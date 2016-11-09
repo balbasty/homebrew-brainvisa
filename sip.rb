50dbd654e0f45926862e3d689be8894db1c28b1f
ybalbastre <yael.balbastre@gmail.com>
2016-11-09 15:11:31 +0100

    Add formula for sip 4.15

---
+++ sip.rb
@@ -0,0 +1,90 @@
+class Sip4155 < Formula
+  desc "Tool to create Python bindings for C and C++ libraries"
+  homepage "https://www.riverbankcomputing.com/software/sip/intro"
+  url "https://downloads.sourceforge.net/project/pyqt/sip/sip-4.15.5/sip-4.15.5.tar.gz"
+  sha256 "9b11d87e7f382ce22bf013137f29462e75b56f5ea46f3fea6061bb20f967d559"
+
+  option "without-python", "Build without python2 support"
+  depends_on :python => :recommended if MacOS.version <= :snow_leopard
+  depends_on :python3 => :optional
+
+  def install
+    if build.without?("python3") && build.without?("python")
+      odie "sip: --with-python3 must be specified when using --without-python"
+    end
+
+    Language::Python.each_python(build) do |python, version|
+      # Note the binary `sip` is the same for python 2.x and 3.x
+      system python, "configure.py",
+                     "--deployment-target=#{MacOS.version}",
+                     "--destdir=#{lib}/python#{version}/site-packages",
+                     "--bindir=#{bin}",
+                     "--incdir=#{include}",
+                     "--sipdir=#{HOMEBREW_PREFIX}/share/sip"
+      system "make"
+      system "make", "install"
+      system "make", "clean"
+    end
+  end
+
+  def post_install
+    (HOMEBREW_PREFIX/"share/sip").mkpath
+  end
+
+  def caveats; <<-EOS.undent
+    The sip-dir for Python is #{HOMEBREW_PREFIX}/share/sip.
+  EOS
+  end
+
+  test do
+    (testpath/"test.h").write <<-EOS.undent
+      #pragma once
+      class Test {
+      public:
+        Test();
+        void test();
+      };
+    EOS
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include "test.h"
+      #include <iostream>
+      Test::Test() {}
+      void Test::test()
+      {
+        std::cout << "Hello World!" << std::endl;
+      }
+    EOS
+    (testpath/"test.sip").write <<-EOS.undent
+      %Module test
+      class Test {
+      %TypeHeaderCode
+      #include "test.h"
+      %End
+      public:
+        Test();
+        void test();
+      };
+    EOS
+    (testpath/"generate.py").write <<-EOS.undent
+      from sipconfig import SIPModuleMakefile, Configuration
+      m = SIPModuleMakefile(Configuration(), "test.build")
+      m.extra_libs = ["test"]
+      m.extra_lib_dirs = ["."]
+      m.generate()
+    EOS
+    (testpath/"run.py").write <<-EOS.undent
+      from test import Test
+      t = Test()
+      t.test()
+    EOS
+    system ENV.cxx, "-shared", "-Wl,-install_name,#{testpath}/libtest.dylib",
+                    "-o", "libtest.dylib", "test.cpp"
+    system bin/"sip", "-b", "test.build", "-c", ".", "test.sip"
+    Language::Python.each_python(build) do |python, version|
+      ENV["PYTHONPATH"] = lib/"python#{version}/site-packages"
+      system python, "generate.py"
+      system "make", "-j1", "clean", "all"
+      system python, "run.py"
+    end
+  end
+end
