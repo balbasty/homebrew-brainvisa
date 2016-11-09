35df5db81c99c237c6bf461c6edc735fb7576a4c
ybalbastre <yael.balbastre@gmail.com>
2016-11-09 15:35:13 +0100

    Add formula for libsigc++ 2.4

---
+++ libsigc++.rb
@@ -0,0 +1,29 @@
+class Libsigcxx < Formula
+  desc "Callback framework for C++"
+  homepage "http://libsigc.sourceforge.net"
+  url "https://download.gnome.org/sources/libsigc++/2.4/libsigc++-2.4.1.tar.xz"
+  sha256 "540443492a68e77e30db8d425f3c0b1299c825bf974d9bfc31ae7efafedc19ec"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <sigc++/sigc++.h>
+
+      void somefunction(int arg) {}
+
+      int main(int argc, char *argv[])
+      {
+         sigc::slot<void, int> sl = sigc::ptr_fun(&somefunction);
+         return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp",
+                   "-L#{lib}", "-lsigc-2.0", "-I#{include}/sigc++-2.0", "-I#{lib}/sigc++-2.0/include", "-o", "test"
+    system "./test"
+  end
+end
